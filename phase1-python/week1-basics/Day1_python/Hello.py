f500c1c21b77769cd299aba2c75dc4dc07cb5d04
veeranbu <anbuveerapandiyan@gmail.com>
2026-03-16 16:34:25 +0530

    Day1_starting

--- phase1-python/week1-basics/Day1_python/Hello.py
+++ phase1-python/week1-basics/Day1_python/Hello.py
@@ -1,1 +1,23 @@
 # Introduction
+#Day1- Python Basics
+from math import floor
+
+print("Hello World") #print helloworld program
+print(99+99) #addition
+print(99-93) #subtraction
+print(99*2)  #multiplication
+print(100/5)  #Divide
+print(100**2) #exponentital
+print(100//2) #floor
+print(100%2) #modulus
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('anbu'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(True))
+
+
